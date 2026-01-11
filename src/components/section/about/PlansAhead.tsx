ab4e655f7ce1d3ae013c3edac4f4dc375f7193e2
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-01-11 11:02:25 +0700

    Added data for about

--- src/components/section/about/PlansAhead.tsx
+++ src/components/section/about/PlansAhead.tsx
@@ -1,31 +1,75 @@
-import { useState } from "react"
-
-const text = {
-	
-}
+import { useEffect, useState } from "react"
+import { Icon } from "@iconify/react"
+import aboutData from "@/data/about.json"
 
 export default function YearsPlanAhead(){
 	const [year, setYear] = useState<2026 | 2027 | 2028>(2026);
 	const [tab, setTab] = useState<"Roadmap" | "Personal" | "Why">("Roadmap");
+	const changeTab = (tab: "Roadmap" | "Personal" | "Why") => {
+		setTab(tab);
+	};
+	const changeYear = (year: 2026 | 2027 | 2028) => {
+		setYear(year);
+	};
+	useEffect(() => {
+		switch (tab){
+			case "Roadmap":
+				
+		}
+	}, [year, tab])
 	return (
-		<div 
-			className="bg-accent text-card-foreground  border-muted flex flex-col gap-6 overflow-hidden rounded-xl border shadow-sm xl:flex-row-reverse"
+		<section className="section flex-col flex gap-2">
+			<h2>📖 My 2.5 Years Plan</h2>
+			<div 
+			className="bg-accent text-card-foreground border-muted flex flex-col gap-6 overflow-hidden rounded-xl border shadow-sm "
 		>
-			<div className="flex flex-row xl:flex-col w-full items-center justify-center gap-[16%]">
-				<button>2026</button>
-				<button>2027</button>
-				<button>2028</button>
+			<div className="w-full justify-between flex flex-row bg-muted px-12 py-8">
+				<button 
+					className={`${tab === "Roadmap" ? "bg-background text-foreground" : "text-foreground-muted hover:text-foreground"} flex min-h-8 min-w-24 items-center justify-center rounded-xl px-4 py-2 transition-transform duration-300 hover:scale-105 gap-2`}
+					onClick={() => changeTab("Roadmap")}
+				>
+					<Icon className={`${tab === "Roadmap" && "text-primary"}`} icon="tabler:road"/>
+					Roadmap
+				</button>
+				<button 
+					className={`${tab === "Personal" ? "bg-background text-foreground" : "text-foreground-muted hover:text-foreground"} flex min-h-8 min-w-24 items-center justify-center rounded-xl px-4 py-2 transition-transform duration-300 hover:scale-105 gap-2`} 
+					onClick={() => changeTab("Personal")}
+				>
+					<Icon className={`${tab === "Personal" && "text-primary"}`} icon="tabler:trophy"/>
+					Personal
+				</button>
+				<button 
+					className={`${tab === "Why" ? "bg-background text-foreground" : "text-foreground-muted hover:text-foreground"} flex min-h-8 min-w-24 items-center justify-center rounded-xl px-4 py-2 transition-transform duration-300 hover:scale-105 gap-2`}
+					onClick={() => changeTab("Why")}
+				>
+					<Icon className={`${tab === "Why" && "text-primary"}`} icon="tabler:question-mark"/>
+					Why
+				</button>
 			</div>
-			<div className="w-full h-fit flex flex-col">
-				<div className="flex flex-row w-full justify-between px-8">
-					<button>Roadmap</button>
-					<button>Personal</button>
-					<button>Why</button>
-				</div>
-				<div >
-
-				</div>
+			<div className="h-80 flex flex-col overflow-y-auto px-12 py-8">
+				{}
+			</div>
+			<div className="w-full justify-between flex flex-row bg-muted px-12 py-8">
+				<button 
+					className={`${year === 2026 ? "bg-background text-foreground" : "text-foreground-muted hover:text-foreground"} flex min-h-8 min-w-24 items-center justify-center rounded-xl px-4 py-2 transition-transform duration-300 hover:scale-105 gap-2`}
+					onClick={() => changeYear(2026)}
+				>
+					2026
+				</button>
+				<button 
+					className={`${year === 2027 ? "bg-background text-foreground" : "text-foreground-muted hover:text-foreground"} flex min-h-8 min-w-24 items-center justify-center rounded-xl px-4 py-2 transition-transform duration-300 hover:scale-105 gap-2`}
+					onClick={() => changeYear(2027)}
+				>
+					2027
+				</button>
+				<button
+					className={`${year === 2028 ? "bg-background text-foreground" : "text-foreground-muted hover:text-foreground"} flex min-h-8 min-w-24 items-center justify-center rounded-xl px-4 py-2 transition-transform duration-300 hover:scale-105 gap-2`}
+					onClick={() => changeYear(2028)}
+				>
+					2028
+				</button>
 			</div>
 		</div>
+		</section>
 	)
 }
