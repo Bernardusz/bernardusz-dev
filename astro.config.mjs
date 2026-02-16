1fb2c6e58605f5e862d3124f6157c30edeb9f77b
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-02-16 10:14:25 +0700

    Fixed Perfromance and Accesibility issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
 
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
@@ -10,17 +11,19 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-
+	site: 'https://bernardusz-dev.vercel.app', 
   vite: {
     resolve: {
       alias: {
-        "@": "/src",
-        "~": "/public",
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "~": fileURLToPath(new URL("./src/assets", import.meta.url)),
       },
     },
     plugins: [tailwindcss()],
   },
-
+  build: {
+	inlineStylesheets: "always"
+  },
   adapter: vercel({
     imageService: true,
     webAnalytics: {
